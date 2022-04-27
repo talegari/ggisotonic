aec69e78e198c3cefcd4f7300d6b5a453554e0f7
talegari <sri.teach@gmail.com>
2022-04-28 00:34:13 +0530

    first cran release

--- R/isotonic.R
+++ R/isotonic.R
@@ -3,34 +3,46 @@
 StatMonotonic = ggplot2::ggproto(
     "StatMonotonic",
     ggplot2::Stat,
-    required_aes = c("x", "y", "w"),
+    required_aes = c("x", "y"),
+    optional_aes = c("w"),
 
-    compute_group = function(data, scales, params, precision) {
+    compute_group = function(data, scales, params, precision, type) {
 
-        if (all(data$w == 1)) {
-            df = data.frame("x" = data$x, "y" = data$y) %>%
-                    na.omit() %>%
-                    dplyr::mutate(x = round(x, precision)) %>%
-                    dplyr::group_by(x) %>%
-                    dplyr::summarise(y = mean(y)) %>%
-                    dplyr::ungroup() %>%
-                    dplyr::arrange(x)
-
-            res_monoreg = fdrtool::monoreg(x = df$x, y = df$y)
-
-        } else {
-
-            df = data.frame("x" = data$x, "y" = data$y, "w" = data$w) %>%
-                    dplyr::mutate(x = round(x, precision)) %>%
-                    dplyr::group_by(x) %>%
-                    dplyr::summarise(y = weighted.mean(y, w),
-                                     w = mean(w)
-                    ) %>%
-                    dplyr::ungroup() %>%
-                    dplyr::arrange(x)
-
-            res_monoreg = fdrtool::monoreg(x = df$x, y = df$y, w = df$w)
+        # error when 'w' is a non-numeric column
+        if (!is.null(data$w) && !is.numeric(data$w)) {
+            stop("weight column should be numeric")
         }
+
+        # determine weights
+        equal_weight = TRUE # equal weights
+        if (!is.null(data$w) && is.numeric(data$w)) {
+            equal_weight = FALSE # weights are provided
+        }
+
+        if (equal_weight) {
+            df = data.frame("x" = data$x, "y" = data$y, "w" = 1)
+        } else {
+            df = data.frame("x" = data$x, "y" = data$y, "w" = data$w)
+        }
+
+        # aggregate one row per given 'x'
+        df = df %>%
+            na.omit() %>%
+            dplyr::mutate(x = round(x, precision)) %>%
+            dplyr::group_by(x) %>%
+            dplyr::summarise(y = weighted.mean(y, w),
+                             w = mean(w)
+                             ) %>%
+            dplyr::ungroup() %>%
+            dplyr::arrange(x)
+
+        # fit isotonic regression
+        res_monoreg = fdrtool::monoreg(x = df$x,
+                                       y = df$y,
+                                       w = df$w,
+                                       type = type
+                                       )
+
 
         res = data.frame("x" = df$x, "y" = res_monoreg$yf)
         return(res)
@@ -44,23 +56,25 @@ StatMonotonic = ggplot2::ggproto(
 #'   `fdrtool::monoreg` with optional weights
 #' @inheritParams ggplot2::stat_identity
 #' @param precision Round 'x' with some precision to remove duplicates values
+#' @param increasing (bool) Whether y increases with x (isotonic)
 #' @examples
 #' library("ggplot2")
+#' set.seed(100)
 #' dataset = data.frame(x = sort(runif(1e2)),
-#'                      y = c(rnorm(1e2/2), rnorm(1e2/2, mean = 4)),
-#'                      w = sample(1:3, 1e2, replace = TRUE)
-#'                      )
+#'                   y = c(rnorm(1e2/2), rnorm(1e2/2, mean = 4)),
+#'                   w = sample(1:3, 1e2, replace = TRUE)
+#'                   )
 #'
-#' # plot unweighted isotonic regression line
+#' # plot isotonic regression line
 #' ggplot(dataset, aes(x = x, y = y)) +
-#'     geom_point() +
-#'     stat_isotonic(aes(w = 1))
+#'  geom_point() +
+#'  stat_isotonic()
 #'
-#' # plot weighted isotonic regression line
+#' # plot weighted isotonic regression line along with facets
 #' ggplot(dataset, aes(x = x, y = y)) +
-#'     geom_point() +
-#'     stat_isotonic(aes(w = w, color = 'red'), show.legend = FALSE) +
-#'     facet_wrap(w ~ .)
+#'  geom_point() +
+#'  stat_isotonic(aes(w = w), color = 'red', size = 1.5, show.legend = FALSE) +
+#'  facet_wrap(w ~ .)
 #'
 stat_isotonic = function(mapping = NULL,
                           data = NULL,
@@ -69,7 +83,27 @@ stat_isotonic = function(mapping = NULL,
                           show.legend = NA,
                           inherit.aes = TRUE,
                           precision = 4,
+                          increasing = TRUE,
                           ...){
+
+    # increasing should be a bool
+    if (!(is.logical(increasing) && length(increasing) == 1)) {
+        stop("increasing should be TRUE or FALSE")
+    }
+
+    if (increasing) {
+        type = "isotonic"
+    } else {
+        type = "antitonic"
+    }
+
+    if (!(precision > 1 &&
+          length(precision == 1) &&
+          as.integer(precision) == precision
+          )
+        ) {
+        stop("precision should be an positive integer")
+    }
 
     ggplot2::layer(stat = StatMonotonic,
                    data = data,
@@ -78,7 +112,10 @@ stat_isotonic = function(mapping = NULL,
                    position = position,
                    show.legend = show.legend,
                    inherit.aes = inherit.aes,
-                   params = list(precision = precision, ...)
+                   params = list(precision = precision,
+                                 type = type,
+                                 ...
+                                 )
     )
 }
 
--- README.md
+++ README.md
@@ -11,6 +11,11 @@ or monotonic regression curves similar to `ggplot2::geom_smooth`.
 
 ## Installation
 
+``` r
+install.packages("ggisotonic")
+library("ggisotonic")
+```
+
 You can install the released version of ggisotonic from github with:
 
 ``` r
@@ -20,27 +25,37 @@ remotes::install_github("talegari/ggisotonic")
 ## Example
 
 ``` r
-library(ggisotonic)
 library("ggplot2")
+set.seed(100)
 dataset = data.frame(x = sort(runif(1e2)),
-                     y = c(rnorm(1e2/2), rnorm(1e2/2, mean = 4)),
-                     w = sample(1:3, 1e2, replace = TRUE)
-                     )
-
-# plot unweighted isotonic regression line
-ggplot(dataset, aes(x = x, y = y)) +
-    geom_point() +
-    stat_isotonic(aes(w = 1))
+                    y = c(rnorm(1e2/2), rnorm(1e2/2, mean = 4)),
+                    w = sample(1:3, 1e2, replace = TRUE)
+                    )
+print(head(dataset))
+#>            x          y w
+#> 1 0.03014575 -0.4470622 3
+#> 2 0.03780258 -1.7385979 2
+#> 3 0.05638315  0.1788648 2
+#> 4 0.09151028  1.8974657 3
+#> 5 0.12348723 -2.2719255 3
+#> 6 0.12523909  0.9804641 2
 ```
-
-<img src="man/figures/README-example-1.png" width="100%" />
 
 ``` r
-# plot weighted isotonic regression line
+# plot isotonic regression line
 ggplot(dataset, aes(x = x, y = y)) +
-    geom_point() +
-    stat_isotonic(aes(w = w, color = 'red'), show.legend = FALSE) +
-    facet_wrap(w ~ .)
+   geom_point() +
+   stat_isotonic()
 ```
 
-<img src="man/figures/README-example-2.png" width="100%" />
+<img src="man/figures/README-unnamed-chunk-4-1.png" width="100%" />
+
+``` r
+# plot weighted isotonic regression line along with facets
+ggplot(dataset, aes(x = x, y = y)) +
+   geom_point() +
+   stat_isotonic(aes(w = w), color = 'red', size = 1.5, show.legend = FALSE) +
+   facet_wrap(w ~ .)
+```
+
+<img src="man/figures/README-unnamed-chunk-4-2.png" width="100%" />
